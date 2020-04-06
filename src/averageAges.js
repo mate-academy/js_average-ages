8b3a74649bcfb6830017178d1b7416358b862353
Andrey Faychak <faychak333@gmail.com>
2020-04-06 20:52:38 +0300

    'solution'

--- src/averageAges.js
+++ src/averageAges.js
@@ -15,11 +15,31 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with logical operators (&&, ||) or ternary operator (?:)
-  // without nesting
+  let result = 0;
+  let arr;
+
+  if (century !== undefined) {
+    arr = people.filter(man => man.sex === 'm')
+      .filter(manDied => Math.ceil(manDied.died / 100) === century);
+
+    arr.forEach(person => {
+      result += person.died - person.born;
+
+      return person;
+    });
+
+    return result / arr.length;
+  } else {
+    arr = people.filter(man => man.sex === 'm');
+
+    arr.forEach(person => {
+      result += person.died - person.born;
+
+      return person;
+    });
+
+    return result / arr.length;
+  }
 }
 
 /**
@@ -35,6 +55,31 @@ function calculateMenAverageAge(people, century) {
  */
 function calculateWomenAverageAge(people, withChildren) {
   // write code here
+  let result = 0;
+  let arr;
+
+  if (withChildren !== undefined) {
+    arr = people.filter(women => women.sex === 'f')
+      .filter(mama => people.some(mam => mam.mother === mama.name) === true);
+
+    arr.forEach(person => {
+      result += person.died - person.born;
+
+      return person;
+    });
+
+    return result / arr.length;
+  } else {
+    arr = people.filter(women => women.sex === 'f');
+
+    arr.forEach(person => {
+      result += person.died - person.born;
+
+      return person;
+    });
+
+    return result / arr.length;
+  }
 }
 
 /**
@@ -53,10 +98,54 @@ function calculateWomenAverageAge(people, withChildren) {
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
   // write code here
+  let result = 0;
+  let count = 0;
+  let arr;
+
+  if (onlyWithSon !== undefined) {
+    arr = people.filter(women => women.sex === 'f')
+      .filter(mama => people
+        .some(child => (child.mother === mama.name
+          && child.sex === 'm') === true));
+
+    arr.forEach(person => {
+      people.forEach(child => {
+        if (child.mother === person.name && child.sex === 'm') {
+          result += child.born - person.born;
+          count++;
+        }
+
+        return child;
+      });
+
+      return person;
+    });
+
+    return result / count;
+  } else {
+    arr = people.filter(women => women.sex === 'f')
+      .filter(mama => people
+        .some(child => (child.mother === mama.name) === true));
+
+    arr.forEach(person => {
+      people.forEach(child => {
+        if (child.mother === person.name) {
+          result += child.born - person.born;
+          count++;
+        }
+
+        return child;
+      });
+
+      return person;
+    });
+
+    return result / count;
+  }
 }
 
 module.exports = {
   calculateMenAverageAge,
   calculateWomenAverageAge,
-  calculateAverageAgeDiff
+  calculateAverageAgeDiff,
 };
