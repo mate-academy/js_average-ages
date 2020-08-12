ddbc6c873d35b265ac4cbabee313f2213470922d
nanccyy <lidiapriymak@gmail.com>
2020-08-12 22:09:43 +0800

    changes

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,47 +1,35 @@
 'use strict';
 
 function calculateMenAverageAge(people, century) {
-  const average = people
+  return +people
     .filter(person => (century > 0)
       ? person.sex === 'm' && Math.ceil(person.died / 100) === century
-      : person.sex === 'm');
-
-  return +average
+      : person.sex === 'm')
     .map(person => person.died - person.born)
     .reduce((accum, value, index, array) => accum + value / array.length, 0)
     .toFixed(2);
 }
 
 function calculateWomenAverageAge(people, withChildren) {
-  const average = people
+  return +people
     .filter(person => (withChildren)
       ? people.some(child => child.mother === person.name)
-      : person.sex === 'f');
-
-  return +average
+      : person.sex === 'f')
     .map(person => person.died - person.born)
     .reduce((accum, value, index, array) => accum + value / array.length, 0)
     .toFixed(2);
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const average = [];
-
   const children = people
     .filter(child =>
       people.some(mother => (onlyWithSon)
         ? child.mother === mother.name && child.sex === 'm'
         : child.mother === mother.name));
 
-  people.map(mother => {
-    children.map(child => {
-      if (mother.name === child.mother) {
-        average.push(child.born - mother.born);
-      }
-    });
-  });
-
-  return +average
+  return +children
+    .map(child => child.born - people
+      .find(person => person.name === child.mother).born)
     .reduce((acc, item, index, array) => acc + item / array.length, 0)
     .toFixed(2);
 }
