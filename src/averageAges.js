deb36a505502ccb5bc0ae1423b505da9e4f92c33
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-17 16:52:09 +0200

    six commit

--- src/averageAges.js
+++ src/averageAges.js
@@ -21,12 +21,11 @@ function calculateMenAverageAge(people, century) {
   // replace `if ()` statement with &&, || or ?:
   // without nesting
   const men = people.filter(person => person.sex === 'm');
-  const ages = men.reduce((sum, person) =>
-    sum + (person.died - person.born), 0)
+  const ages = men.reduce((sum, person) => sum + (person.died - person.born), 0)
     / men.length;
 
-  const mansWithCentury = men.filter(person => Math.ceil(person.died / 100)
-  === century);
+  const mansWithCentury = men.filter(person => Math
+    .ceil(person.died / 100) === century);
 
   const agesWithCentury = mansWithCentury.reduce((sum, person) =>
     sum + (person.died - person.born), 0)
