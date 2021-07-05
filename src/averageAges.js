cc2501a7d8c3dd4c856a89d57548d2cbed56ebcd
Mykola shcherbak <hohotun21121987@gmail.com>
2021-07-05 14:19:39 +0300

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -18,10 +18,11 @@ function calculateMenAverageAge(people, century) {
   const male = people.filter(person => century
     ? Math.ceil(person.died / 100) === century
     && person.sex === 'm'
-    : person.sex === 'm');
+    : person.sex === 'm'
+  );
 
-  return male.map(man => man.died - man.born)
-    .reduce((a, b) => a + b) / male.length;
+  return male.reduce((acc, man) => acc + (man.died - man.born), 0)
+  / male.length;
 }
 
 /**
@@ -39,11 +40,11 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  const female = people.filter(person =>
-    withChildren
-      ? person.sex === 'f'
-      && people.some(kid => kid.mother === person.name)
-      : person.sex === 'f');
+  const female = people.filter(person => withChildren
+    ? person.sex === 'f'
+    && people.some(kid => kid.mother === person.name)
+    : person.sex === 'f'
+  );
 
   return female.map(woman => woman.died - woman.born)
     .reduce((a, b) => a + b) / female.length;
@@ -64,13 +65,15 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const kids = people.filter(person =>
-    !onlyWithSon
-      ? people.find(kid => kid.name === person.mother)
-      : people.find(kid => kid.name === person.mother) && person.sex === 'm');
+  const kids = people.filter(person => !onlyWithSon
+    ? people.find(kid => kid.name === person.mother)
+    : people.find(kid => kid.name === person.mother) && person.sex === 'm');
 
-  return kids.map(kid => kid.born - people.find(women => women.name
-    === kid.mother).born).reduce((a, b) => a + b) / kids.length;
+  const differencies = kids.map(kid => kid.born - people
+    .find(women => women.name === kid.mother).born);
+
+  return differencies.reduce((acc, difference) =>
+    acc + difference, 0) / kids.length;
 }
 
 module.exports = {
