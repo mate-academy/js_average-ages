5b0242a1357d9f141357ca8f6be0c9744178e7a2
David Dohru <tronsmartmy@gmail.com>
2021-07-06 21:07:59 +0300

    fixed nesting and entered functions in the variable

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,41 +1,6 @@
 'use strict';
 
-function calculateMenAverageAge(people, century) {
-  let ageOfPassage = [];
-
-  (century === undefined)
-    ? (people.map(item => (item.sex === 'm')
-      && ageOfPassage.push(item.died - item.born)))
-    : (people.map(item =>
-      (item.sex === 'm' && Math.ceil(item.died / 100) === century)
-      && ageOfPassage.push(item.died - item.born)));
-
-  ageOfPassage = calulateAvarageAge(ageOfPassage);
-
-  return ageOfPassage;
-}
-
-function calculateWomenAverageAge(people, withChildren) {
-  let womenAverageAge = [];
-  let resultOfAverageAge = 0;
-  const objWoman = filterIfFemale([...people]);
-
-  objWoman.map(value => (withChildren === undefined)
-    ? womenAverageAge.push(value.died - value.born) : false);
-
-  objWoman.map(item => {
-    people.map(value => (item.name === value.mother && withChildren === true)
-      ? womenAverageAge.push(item.died - item.born) : false);
-  });
-
-  if (withChildren === true) {
-    womenAverageAge = (womenAverageAge
-      .filter((x, index, array) => array.indexOf(x) === index));
-  }
-  resultOfAverageAge = calulateAvarageAge(womenAverageAge);
-
-  return resultOfAverageAge;
-}
+/* ---------------------------------------------------------------- */
 
 const calulateAvarageAge = (womenAverageAgeay) => {
   const womenAverageAgeLength = womenAverageAgeay.length;
@@ -44,26 +9,81 @@ const calulateAvarageAge = (womenAverageAgeay) => {
 
   return resultOfAverageAge;
 };
-const filterIfFemale = (womenAverageAgeay) => womenAverageAgeay
-  .filter(people => people.sex === 'f');
 
-function calculateAverageAgeDiff(people, onlyWithSon) {
-  let allFemaleProvided = [];
-  const dateDifference = [];
+/* ---------------------------------------------------------------- */
 
-  (onlyWithSon === true)
-    ? allFemaleProvided = people.filter(child => child.sex === 'm'
-    && people.some(mother => mother.name === child.mother))
-    : allFemaleProvided = people.filter(child => people.find(
-      mother => mother.name === child.mother));
+function calculateMenAverageAge(people, century) {
+  const withoutCentury = [];
+  const withCentury = [];
+  const allDateWithoutCentury = people.map(item => {
+    (item.sex === 'm')
+      && withoutCentury.push(item.died - item.born);
 
-  allFemaleProvided.map(item => {
-    people.map(value => (value.name === item.mother)
-      ? dateDifference.push(item.born - value.born) : false,
-    );
+    return withoutCentury;
+  });
+  const allDataWithCentury = people.map(item => {
+    (item.sex === 'm' && Math.ceil(item.died / 100) === century)
+          && withCentury.push(item.died - item.born);
+
+    return withCentury;
   });
 
-  return calulateAvarageAge(dateDifference);
+  return century ? calulateAvarageAge(allDataWithCentury[0])
+    : calulateAvarageAge(allDateWithoutCentury[0]);
+}
+
+/* ---------------------------------------------------------------- */
+
+function calculateWomenAverageAge(people, withChildren) {
+  const womenWithChild = [];
+  const womenWithoutChild = [];
+
+  const allWoman = people.filter(person => person.sex === 'f');
+
+  const allWomenLifeTim = allWoman.map(value => {
+    womenWithoutChild.push(value.died - value.born);
+
+    return womenWithoutChild;
+  });
+
+  const womenLifeTimeWithChild = allWoman.map(item => {
+    people.map(value => {
+      (item.name === value.mother)
+       && womenWithChild.push(item.died - item.born);
+    });
+
+    return womenWithChild;
+  });
+
+  return withChildren
+    ? calulateAvarageAge(womenLifeTimeWithChild[0].filter((x, index, array) =>
+      array.indexOf(x) === index))
+    : calulateAvarageAge(allWomenLifeTim[0]);
+}
+
+/* ---------------------------------------------------------------- */
+
+function calculateAverageAgeDiff(people, onlyWithSon) {
+  const withSunAgeDate = [];
+  const withoutSunAgeDate = [];
+
+  const averageAgeWithSun = people.filter(child => child.sex === 'm'
+  && people.map(mother => mother.name === child.mother));
+  const averageAgeWithoutSun = people.filter(child => people.find(
+    mother => mother.name === child.mother));
+
+  const withSunOrNo = (person, array) => {
+    person.map(item => {
+      people.map(value => (value.name === item.mother)
+        && array.push(item.born - value.born));
+    });
+
+    return array;
+  };
+
+  return onlyWithSon
+    ? calulateAvarageAge(withSunOrNo(averageAgeWithSun, withSunAgeDate))
+    : calulateAvarageAge(withSunOrNo(averageAgeWithoutSun, withoutSunAgeDate));
 }
 
 module.exports = {
