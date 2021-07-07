42c640a9fcb24c49c04dbd76b2d75e8f54317417
Сакевич Анастасія <asakevych@ASMAC.local>
2021-07-07 10:50:44 +0200

    some changes, ps idk how to change without !, it doesnt work

--- src/averageAges.js
+++ src/averageAges.js
@@ -5,9 +5,9 @@ function calculateMenAverageAge(people, century) {
     ? people.filter(person => person.sex === 'm')
     : people.filter(person => person.sex === 'm'
       && Math.ceil(person.died / 100) === century);
-  const menAges = men.map(person => person.died - person.born);
-  const menAverageAge = menAges.reduce((sum, age) => sum + age)
-  / menAges.length;
+
+  const menAverageAge = men.reduce(
+    (sum, person) => sum + (person.died - person.born), 0) / men.length;
 
   return menAverageAge;
 }
@@ -17,26 +17,25 @@ function calculateWomenAverageAge(people, withChildren) {
   const women = (!withChildren)
     ? people.filter(person => person.sex === 'f')
     : people.filter(person => haveChildren.includes(person.name));
-  const womenAges = women.map(person => person.died - person.born);
-  const womenAverageAge = womenAges.reduce((accum, age) => accum + age)
-  / womenAges.length;
+
+  const womenAverageAge = women.reduce(
+    (sum, person) => sum + (person.died - person.born), 0) / women.length;
 
   return womenAverageAge;
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
   const children = (!onlyWithSon)
-    ? people.filter(person => people.some(parent =>
-      person.mother === parent.name))
-    : people.filter(person => people.some(parent =>
-      person.mother === parent.name && person.sex === 'm'));
+    ? people.filter(person => people.some(
+      parent => person.mother === parent.name))
+    : people.filter(person => people.some(
+      parent => person.mother === parent.name && person.sex === 'm'));
 
-  const haveChildren = children.map(child => people.find(mother =>
-    mother.name === child.mother));
-  const differenceAges = children.map((child, i) =>
-    child.born - haveChildren[i].born);
-  const averageAge = differenceAges.reduce((accum, age) => accum + age)
-  / differenceAges.length;
+  const haveChildren = children.map(child => people.find(
+    mother => mother.name === child.mother));
+  const averageAge = children.map(
+    (child, i) => child.born - haveChildren[i].born)
+    .reduce((sum, age) => sum + age) / haveChildren.length;
 
   return averageAge;
 }
