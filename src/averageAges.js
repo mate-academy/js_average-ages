f51290b6560fefc69e4122ca5c8f93eb7972816c
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-11 15:38:38 +0200

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,22 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+  const menAge = [];
+
+  people.filter(men => men.sex === 'm'
+    ? menAge.push(men.died - men.born)
+    : men);
+
+  const menAgeCentury = [];
+
+  people.filter(men => men.sex === 'm'
+    && Math.ceil(men.died / 100) === century
+    ? menAgeCentury.push(men.died - men.born)
+    : men);
+
+  return !century
+    ? menAge.reduce((a, b) => a + b) / menAge.length
+    : menAgeCentury.reduce((a, b) => a + b) / menAgeCentury.length;
 }
 
 /**
@@ -37,7 +53,22 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const womanAge = [];
+
+  people.filter(woman => woman.sex === 'f'
+    ? womanAge.push(woman.died - woman.born)
+    : woman);
+
+  const womanAgeWithChild = [];
+
+  people.filter(woman => woman.sex === 'f'
+    && people.some(y => woman.name === y.mother)
+    ? womanAgeWithChild.push(woman.died - woman.born)
+    : woman);
+
+  return !withChildren
+    ? womanAge.reduce((a, b) => a + b) / womanAge.length
+    : womanAgeWithChild.reduce((a, b) => a + b) / womanAgeWithChild.length;
 }
 
 /**
@@ -55,7 +86,26 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // write code here
+  const mothersAge = [];
+
+  people.filter(mother => mother.sex === 'f'
+    && people.some(child => mother.name === child.mother)
+    ? people.filter(child => mother.name === child.mother
+      ? mothersAge.push(child.born - mother.born)
+      : child)
+    : mother);
+
+  const mothersAgeWithSon = [];
+
+  people.filter(son => son.sex === 'm'
+    && people.some(mother => mother.name === son.mother)
+    ? people.filter(mother => mother.name === son.mother
+      ? mothersAgeWithSon.push(son.born - mother.born)
+      : mother)
+    : son);
+
+  return !onlyWithSon ? mothersAge.reduce((a, b) => a + b) / mothersAge.length
+    : mothersAgeWithSon.reduce((a, b) => a + b) / mothersAgeWithSon.length;
 }
 
 module.exports = {
