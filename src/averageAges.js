5d4c29e70f1d9e2f2b7ef8120fe6a0f47358577a
Igor Kovalev <bizzzy0@gmail.com>
2021-10-14 10:22:47 +0300

    Task was solved =/

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,61 +1,87 @@
+/* eslint-disable no-unused-expressions */
 'use strict';
 
-/**
- * Implement calculateMenAverageAge function
- *
- * Function returns average age of men in array. If `century` is specified then
- * function calculates average age only for men who died in this century
- *
- * To calculate century:
- * Divide year of person's death by 100: Math.ceil(person.died / 100)
- *
- * @param {object[]} people
- * @param {number} century - optional
- *
- * @return {number}
- */
+function getRoundNumber(number) {
+  return +number.toFixed(2);
+}
+
+function getHumanAge(human) {
+  const { born, died } = human;
+  const humanAge = died - born;
+
+  return humanAge;
+}
+
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with &&, || or ?:
-  // without nesting
+  const manList = people.filter(human => human.sex === 'm');
+  let manCount;
+  let manAgeList;
+  let manAverageAge;
+  let manDiedInThisCentury;
+
+  (century) ? (
+    manDiedInThisCentury = manList.filter(function(man) {
+      const { died } = man;
+
+      return Math.ceil(died / 100) === century;
+    }),
+    manCount = manDiedInThisCentury.length,
+    manAgeList = manDiedInThisCentury.map(getHumanAge),
+    manAverageAge = manAgeList.reduce(
+      (current, next) => current + next) / manCount
+  ) : (
+    manCount = manList.length,
+    manAgeList = manList.map(getHumanAge),
+    manAverageAge = manAgeList.reduce(
+      (current, next) => current + next) / manCount
+  );
+
+  return getRoundNumber(manAverageAge);
 }
 
-/**
- * Implement calculateWomenAverageAge function
- *
- * Function returns average ave of women in array. If `withChildren` is
- * specified then function calculates average age only for women with children
- *
- * Hint: To check if a woman has children you should find the other who mention
- * her as mother.
- *
- * @param {object[]} people
- * @param {boolean} withChildren - optional
- *
- * @return {number}
- */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const womenList = people.filter(human => human.sex === 'f');
+  let womenCount;
+  let womenAgeList;
+  let womenAverageAge;
+  let filteredPeopleList;
+  let motherList;
+  let womenWhichHaveChild;
+
+  (withChildren) ? (
+    filteredPeopleList = people.filter(women => women.mother),
+    motherList = Array.from(new Set(filteredPeopleList.map(
+      human => human.mother
+    ))),
+    womenWhichHaveChild = womenList.filter(
+      women => motherList.includes(women.name)
+    ),
+    womenCount = womenWhichHaveChild.length,
+    womenAgeList = womenWhichHaveChild.map(getHumanAge),
+    womenAverageAge = womenAgeList.reduce(
+      (current, next) => current + next) / womenCount
+  ) : (
+    womenCount = womenList.length,
+    womenAgeList = womenList.map(getHumanAge),
+    womenAverageAge = womenAgeList.reduce(
+      (current, next) => current + next) / womenCount
+  );
+
+  return getRoundNumber(womenAverageAge);
 }
 
-/**
- * Implement calculateAverageAgeDiff function.
- *
- * The function returns an average age difference between a mother and her
- * child in the array. (A mother's age at child birth)
- *
- * If `onlyWithSon` is specified then function calculates age difference only
- * for mothers who have son.
- *
- * @param {object[]} people
- * @param {boolean} onlyWithSon - optional
- *
- * @return {number}
- */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // write code here
+  const childrenList = people.filter(human =>
+    people.find(mother => human.mother === mother.name)
+      && (!onlyWithSon || human.sex === 'm'
+      ));
+  const childrenCount = childrenList.length;
+
+  const averageAgeDiff = childrenList.reduce((value, child) =>
+    value + (child.born - people.find(person =>
+      person.name === child.mother).born), 0) / childrenCount;
+
+  return getRoundNumber(averageAgeDiff);
 }
 
 module.exports = {
