7f4ce1b1ec5758a58045d3f89028f331bbac6567
Nikita Polishchuk <theetempleovpsychicyouth@gmail.com>
2021-12-23 19:56:01 +0200

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -15,11 +15,24 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with &&, || or ?:
-  // without nesting
+  const men = people.filter(person => person.sex === 'm');
+
+  if (century !== undefined) {
+    const menDiedInCentury = men.filter(man =>
+      (Math.ceil(man.died / 100)) === century);
+    const ageOfMenDiedInCentury = menDiedInCentury.map(man =>
+      man.died - man.born);
+    const averageAgeOfDiedInCentury = ageOfMenDiedInCentury.reduce((p, n) =>
+      p + n) / ageOfMenDiedInCentury.length;
+
+    return averageAgeOfDiedInCentury;
+  }
+
+  const ageOfMen = men.map(man => man.died - man.born);
+  const averageLifespan = ageOfMen.reduce((prev, curr) =>
+    prev + curr) / ageOfMen.length;
+
+  return averageLifespan;
 }
 
 /**
@@ -37,7 +50,25 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const women = people.filter(person => person.sex === 'f');
+
+  if (withChildren !== undefined) {
+    const mothers = women.filter(person =>
+      people.some(child => child.mother === person.name));
+    const ageOfMother = mothers.map(woman =>
+      woman.died - woman.born);
+    const averageLifespanMother = ageOfMother.reduce((p, n) =>
+      p + n) / ageOfMother.length;
+
+    return averageLifespanMother;
+  }
+
+  const ageOfWomen = women.map(woman =>
+    woman.died - woman.born);
+  const averageLifespan = ageOfWomen.reduce((p, n) =>
+    p + n) / ageOfWomen.length;
+
+  return averageLifespan;
 }
 
 /**
@@ -55,7 +86,25 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // write code here
+  const children = people.filter(person =>
+    people.some(mother => person.mother === mother.name));
+  const mothers = people.filter(person =>
+    children.some(child => child.mother === person.name));
+  const motherOfChild = (child) => {
+    return mothers.find(mother =>
+      child.mother === mother.name);
+  };
+
+  if (onlyWithSon) {
+    const sons = children.filter(person =>
+      person.sex === 'm');
+
+    return sons.reduce((p, n) =>
+      p + (n.born - motherOfChild(n).born), 0) / sons.length;
+  }
+
+  return children.reduce((p, n) =>
+    p + (n.born - motherOfChild(n).born), 0) / children.length;
 }
 
 module.exports = {
