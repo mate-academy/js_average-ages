f9f50138a9c6ece2072dd64812e2e872926093cd
Olga <arnab@ukr.net>
2022-02-15 21:34:42 +0200

    Final solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -6,9 +6,13 @@ function getAge(deathYear, birthYear) {
 
 function calculateMenAverageAge(people, century) {
   const men = people.filter(
-    person => person.sex === 'm' && (century
-      ? Math.ceil(person.died / 100) === century
-      : true));
+    person => person.sex === 'm'
+    && (
+      century
+        ? Math.ceil(person.died / 100) === century
+        : true
+    )
+  );
 
   return men.reduce(
     (acc, man) => getAge(man.died, man.born) + acc, 0) / men.length;
@@ -16,9 +20,13 @@ function calculateMenAverageAge(people, century) {
 
 function calculateWomenAverageAge(people, withChildren) {
   const women = people.filter(
-    person => person.sex === 'f' && (withChildren
-      ? people.find(child => child.mother === person.name)
-      : true));
+    person => person.sex === 'f'
+    && (
+      withChildren
+        ? people.find(child => child.mother === person.name)
+        : true
+    )
+  );
 
   return women.reduce(
     (acc, woman) => getAge(woman.died, woman.born) + acc, 0) / women.length;
@@ -27,11 +35,16 @@ function calculateWomenAverageAge(people, withChildren) {
 function calculateAverageAgeDiff(people, onlyWithSon) {
   const children = people.filter(
     person => people.find(mother => person.mother === mother.name)
-    && (onlyWithSon
-      ? person.sex === 'm'
-      : true));
+    && (
+      onlyWithSon
+        ? person.sex === 'm'
+        : true
+    )
+  );
 
-  const findMother = name => people.find(mother => name === mother.name);
+  const findMother = name => people.find(
+    mother => name === mother.name
+  );
 
   return children.reduce(
     (acc, child) =>
