f4ee31b315138b88090984dfaf25eed2cb429fd5
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-01 18:27:29 +0300

    2 of 3 implemented

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,6 +1,54 @@
 'use strict';
 
 /**
+ * @param {object[]} arr
+ * @param {string} sex
+ *
+ * @return {object[]}
+ */
+function filterBySex(arr, sex) {
+  return arr
+    .filter(({ 'sex': currSex }) => currSex === sex);
+}
+
+/**
+ * @param {object[]} arr
+ * @param {number} century
+ *
+ * @return {object[]}
+ */
+function filterByCentury(arr, century) {
+  return arr
+    .filter(({ died }) => Math.ceil(died / 100) === century);
+}
+
+/**
+ * @param {number} totalAge
+ * @param {object} man
+ *
+ * @return {number}
+ */
+function sumAges(totalAge, { died, born }) {
+  const age = died - born;
+
+  return totalAge + age;
+}
+
+/**
+ * @param {object[]} arr
+ *
+ * @return {object[]}
+ */
+function getMothers(arr) {
+  const motherNames = arr
+    .filter(({ mother }) => typeof mother === 'string')
+    .map(({ mother }) => mother);
+
+  return arr
+    .filter(({ name }) => motherNames.indexOf(name) !== -1);
+}
+
+/**
  * Implement calculateMenAverageAge function
  *
  * Function returns average age of men in array. If `century` is specified then
@@ -15,11 +63,18 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with &&, || or ?:
-  // without nesting
+  const sex = 'm';
+
+  let men = filterBySex(people, sex);
+
+  if (arguments.length > 1) {
+    men = filterByCentury(men, century);
+  }
+
+  const totalAge = men.reduce(sumAges, 0);
+  const avgAge = totalAge / men.length;
+
+  return avgAge;
 }
 
 /**
@@ -37,7 +92,20 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  let women;
+
+  if (withChildren) {
+    women = getMothers(people);
+  } else {
+    const sex = 'f';
+
+    women = filterBySex(people, sex);
+  }
+
+  const totalAge = women.reduce(sumAges, 0);
+  const avgAge = totalAge / women.length;
+
+  return avgAge;
 }
 
 /**
