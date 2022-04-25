cec5d16a2ea0a4691b2086a1dcc148b04238449d
Roman Lipatov <rlipatov96@gmail.com>
2022-04-25 16:30:05 +0300

    averageAges

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,14 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+
+  const men = people.filter(person => century
+    ? person.sex === 'm' && Math.ceil(person.died / 100) === century
+    : person.sex === 'm'
+  );
+
+  return men.map(person => person.died - person.born)
+    .reduce((a, b) => a + b) / men.length;
 }
 
 /**
@@ -38,6 +46,12 @@ function calculateMenAverageAge(people, century) {
  */
 function calculateWomenAverageAge(people, withChildren) {
   // write code here
+  const women = people.filter(person => withChildren
+    ? people.some(human => human.mother === person.name)
+    : person.sex === 'f');
+
+  return women.map(person => person.died - person.born)
+    .reduce((a, b) => a + b, 0) / women.length;
 }
 
 /**
@@ -56,6 +70,13 @@ function calculateWomenAverageAge(people, withChildren) {
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
   // write code here
+  const children = people.filter(person => onlyWithSon
+    ? people.find(woman => woman.name === person.mother) && person.sex === 'm'
+    : people.find(woman => woman.name === person.mother));
+
+  return children.map(person =>
+    person.born - people.find(mother => person.mother === mother.name).born)
+    .reduce((a, b) => a + b, 0) / children.length;
 }
 
 module.exports = {
