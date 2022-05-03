d53f3ba6779ba36992d9a304d1231991ade46226
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-03 17:22:16 +0300

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -15,7 +15,16 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
+  const mans = people.filter(x => x.sex === 'm');
+  const centMans = mans.filter(x => Math.ceil(x.died / 100) === century);
+
+  return century === undefined
+    ? mans.reduce((sum, x) => sum + (x.died - x.born), 0)
+    / mans.length
+
+    : centMans.reduce((sum, x) => sum + (x.died - x.born), 0)
+    / centMans.length;
+
   // learn how to use array methods like .filter .map .some .every .find .reduce
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
@@ -37,7 +46,17 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const womens = people.filter(x => x.sex === 'f');
+  const mothers = people.filter(x => {
+    return people.some(y => y.mother === x.name);
+  });
+
+  return withChildren === undefined
+    ? womens.reduce((sum, x) => sum + (x.died - x.born), 0)
+    / womens.length
+
+    : mothers.reduce((sum, x) => sum + (x.died - x.born), 0)
+    / mothers.length;
 }
 
 /**
@@ -55,7 +74,32 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // write code here
+  const chields = people.filter(x => {
+    return people.some(y => y.name === x.mother);
+  });
+
+  const chieldsMans = people.filter(x => {
+    return people.some(y => y.name === x.mother) && (x.sex === 'm');
+  });
+
+  // 1. Find childs (depending on onlyWithSon)
+  // 2. Find needed ages
+  // 3. Calculate avergae age
+
+  const childAct = onlyWithSon === undefined
+    ? chields
+    : chieldsMans;
+
+  const age = childAct.map(x => {
+    const motherOfChield = people.find(y => x.mother === y.name);
+
+    return x.born - motherOfChield.born;
+  });
+
+  return (age.reduce((sum, x) => sum + x, 0) / age.length);
+
+  // chield.born - mothers.born
+  //  age. / mothers.lendth
 }
 
 module.exports = {
