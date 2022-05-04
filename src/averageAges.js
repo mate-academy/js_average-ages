b9ccad473ba4aad1f2d5ad388e68dddfb4805402
efdalyalcin <efdalyalcin@hotmail.com>
2022-05-04 16:09:02 +0300

    code cleaned

--- src/averageAges.js
+++ src/averageAges.js
@@ -39,21 +39,6 @@ function calculateWomenAverageAge(people, withChildren) {
   return average;
 }
 
-/**
- * Implement calculateAverageAgeDiff function.
- *
- * The function returns an average age difference between a child and his or her
- * mother in the array. (A mother's age at child birth)
- *
- * If `onlyWithSon` is specified then function calculates age difference only
- * for sons and their mothers.
- *
- * @param {object[]} people
- * @param {boolean} onlyWithSon - optional
- *
- * @return {number}
- */
-
 function calculateAverageAgeDiff(people, onlyWithSon) {
   const mothers = people.filter(person => onlyWithSon
     ? people.find(child => isMother(person, child) && isMale(child))
