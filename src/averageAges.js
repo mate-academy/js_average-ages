17367a3334864e36bc348741030923a3b72a1d89
rostyslav-pyliak <r.pyliak@gmail.com>
2022-05-09 11:16:33 +0300

    finished calculateMenAverageAge function

--- src/averageAges.js
+++ src/averageAges.js
@@ -15,11 +15,14 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with &&, || or ?:
-  // without nesting
+  const men = people.filter(man => man.sex === 'm');
+  const filteredMen = century
+    ? men.filter(person => Math.ceil(person.died / 100) === century)
+    : men;
+
+  return filteredMen
+    .reduce((acc, age) =>
+      ((acc + (age.died - age.born))), 0) / filteredMen.length;
 }
 
 /**
