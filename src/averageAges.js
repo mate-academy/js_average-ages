1e22244503d8b3b0adac7580bb50554ea9f60f5c
Kostya Berehoviy <kostyaber3@gmail.com>
2022-06-04 15:40:47 +0300

    getAveAge solution v02

--- src/averageAges.js
+++ src/averageAges.js
@@ -21,11 +21,13 @@ function calculateMenAverageAge(people, century) {
   // replace `if ()` statement with &&, || or ?:
   // without nesting
 
-  const men = people.filter((person) => century
-    ? Math.ceil(person.died / 100) === century && person.sex === 'm'
-    : person.sex === 'm');
+  const men = people.filter((person) => person.sex === 'm');
 
-  const menAge = men.map((year) => year.died - year.born);
+  const menBornInCentury = century
+    ? men.filter(person => Math.ceil(person.died / 100) === century)
+    : men;
+
+  const menAge = menBornInCentury.map((year) => year.died - year.born);
 
   const wholeMenAverageAge = menAge.reduce(
     (year1, year2) => year1 + year2 / menAge.length, 0);
