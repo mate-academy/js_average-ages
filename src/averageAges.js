5f76359c428cf8780e61198714dc9093a3262d0f
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-17 14:39:11 +0300

    draft v1

--- src/averageAges.js
+++ src/averageAges.js
@@ -14,13 +14,53 @@
  *
  * @return {number}
  */
+
+// const allPeople = require('./people');
+
 function calculateMenAverageAge(people, century) {
   // write code here
   // learn how to use array methods like .filter .map .some .every .find .reduce
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+  // const arrWithMen = people.filter(person => person.sex === 'm');
+
+  // const menAges = arrWithMen.map(age => age.died - age.born);
+
+  // const menAverageAge = menAges.reduce((sumOfAges, currentAge) =>
+  //   sumOfAges + currentAge, 0) / arrWithMen.length;
+  // let diedInCentury = arrWithMen.map(age => Math.ceil(age.died / 100));
+
+  // const filteredPeople = !century
+  //   ? people.filter(person => person.sex === 'm')
+  //   : people.filter(person => person.sex === 'm'
+  //   && Math.ceil(person.died / 100) === century);
+
+  // console.log(filteredPeople);
+  // const averageAge = filteredPeople.reduce((sum, person) =>
+  //   sum + (person.died - person.born), 0) / filteredPeople.length;
+  // console.log(arrWithMen);
+  // console.log(menAverageAge);
+  // console.log(livingInCentury);
+  //  console.log(diedInCentury);
+  // console.log(averageAge);
+  // return menAverageAgeInCentury
+  // if (century === undefined) {
+  //   return menAverageAge;
+  // } else {
+
+  // }
 }
+
+// console.log(calculateMenAverageAge(allPeople, 18));
+// console.log(calculateMenAverageAge('./averageAges'));
+// console.log(calculateMenAverageAge(people, 18));
+// console.log(calculateMenAverageAge(people));
+// console.log(calculateMenAverageAge(require('./averageAges')));
+
+// expect(calculateMenAverageAge(people))
+//     .toBeCloseTo(61.67, 2);
+// });
 
 /**
  * Implement calculateWomenAverageAge function
@@ -36,9 +76,26 @@ function calculateMenAverageAge(people, century) {
  *
  * @return {number}
  */
+
+// const allPeople = require('./people');
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  // const arrWithWomen = people.filter(women => women.sex === 'f');
+  // // console.log(arrWithWomen);
+  // const womenAverageAge = arrWithWomen.map(age => age.died - age.born);
+  // const womenAverageAgeWihtChildrenOnly = arrWithWomen
+  //   .filter(women => women.mother)
+  //   .map(age => age.died - age.born)
+  //   .reduce((sumOfAges, currentAge) => sumOfAges + currentAge)
+  //   / arrWithWomen.length;
+  // console.log(womenAverageAgeWihtChildrenOnly);
+  // // if (withChildren) {
+  // //   return
+  // // }
+  // // console.log(womenAverageAge);
 }
+
+// console.log(calculateWomenAverageAge(allPeople, true));
+// console.log(calculateWomenAverageAge(allPeople ));
 
 /**
  * Implement calculateAverageAgeDiff function.
