1c82da2a50812b13f65ffd29bd281029c1818279
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-18 19:49:19 +0300

    3d function not working

--- src/averageAges.js
+++ src/averageAges.js
@@ -60,8 +60,6 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 
-// const allPeople = require('./people');
-
 function calculateWomenAverageAge(people, withChildren) {
   const womenArr = people.filter(person => person.sex === 'f');
   const womenWithChildren = womenArr.filter((women) => {
@@ -100,9 +98,88 @@ function calculateWomenAverageAge(people, withChildren) {
  *
  * @return {number}
  */
+
+// const allPeople = require('./people');
+
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // let age = people.filter
+  const childrenMother = people.filter(person => {
+    // console.log(person);
+    // избавился от нал в графе матери но остались андефайнд
+    // но почему их нет в ответе.они есть в allChildren  но нет в childrenMother
+    const allChildren = people.find((child) => person.mother === child.name
+    && child.mother !== null);
+    // console.table(allChildren);
+    // let test = allChildren.filter(a => a !== undefined);
+    // console.log(test);
+    // return allChildren;
+
+    return allChildren;
+  })
+    .map(person => {
+      const boyMother = people.find(folk => person.mother === folk.name);
+
+      return {
+        ...person,
+        motherBorn: boyMother.born,
+      };
+    });
+
+  // console.table(childrenMother);
+
+  // названия переменной меняй
+  const chirdrenBoy = people.filter(person => {
+    const boy = people.find((child) => person.mother === child.name
+    && child.mother !== null
+    && person.sex === 'm');
+    // console.table(boy);
+
+    return boy;
+  })
+  // через мап перебираем объект и ищем мам для людей в полном обекте
+  // и потом копируем старый объект ...person,
+  // и добавляем motherBorn: boyMother.born,
+  // это для того чтобы можно было сразу отредюсить один обект без доп поиска
+    .map(person => {
+      const boyMother = people.find(folk => person.mother === folk.name);
+
+      return {
+        ...person,
+        motherBorn: boyMother.born,
+      };
+    });
+
+  // console.table(motherWithBoy);
+
+  const filteredPeople = !onlyWithSon
+    ? childrenMother
+    : chirdrenBoy;
+
+  const averageAge = filteredPeople.map(current =>
+    current.born - current.motherBorn)
+    .reduce((a, b) => a + b, 0) / filteredPeople.length;
+    // console.log(averageAge);
+
+  // const averageAge = filteredPeople.reduce((sum, current) => {
+  //   // console.table(current);
+  //   console.log(sum + (current.born - current.motherBorn));
+  //   let sumAll = sum + (current.born - current.motherBorn)
+  // / filteredPeople.length;
+  //   // console.log(sumAll);
+  //   // return sumAll
+  // })
+
+  // тут надо от рождения ребенка отнять рождение мамы
+  // const averageAge = filteredPeople.reduce((sum, person) =>
+  // sum + (person.born - people.find(child =>
+  // child.mother === child.name)).born, 0) / filteredPeople.length;
+  // const averageAge = filteredPeople.reduce((sum, person) =>
+  //   sum + (person.died - person.born), 0) / filteredPeople.length;
+
+  return averageAge;
 }
+
+// console.log(calculateAverageAgeDiff(allPeople, true));
+// console.log(calculateAverageAgeDiff(allPeople));
 
 module.exports = {
   calculateMenAverageAge,
