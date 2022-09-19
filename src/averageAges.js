91065bcd5a6c9adb23ae7801595b2bdda11aabfe
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-09-19 23:29:39 +0300

    Solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,16 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+
+  const men = (Number.isInteger(century))
+    ? people.filter(person => (person.sex === 'm'
+    && Math.ceil(person.died / 100) === century))
+    : people.filter(person => person.sex === 'm');
+
+  const ageDiedSum = men.reduce((prev, person) => prev + person.died, 0);
+  const ageBornSum = men.reduce((prev, person) => prev + person.born, 0);
+
+  return (ageDiedSum - ageBornSum) / men.length;
 }
 
 /**
@@ -38,6 +48,19 @@ function calculateMenAverageAge(people, century) {
  */
 function calculateWomenAverageAge(people, withChildren) {
   // write code here
+
+  const women = (withChildren)
+    ? people.filter(person => getWomenWithChildren(people, person))
+    : people.filter(person => person.sex === 'f');
+
+  const ageDiedSum = women.reduce((prev, person) => prev + person.died, 0);
+  const ageBornSum = women.reduce((prev, person) => prev + person.born, 0);
+
+  return (ageDiedSum - ageBornSum) / women.length;
+}
+
+function getWomenWithChildren(people, person) {
+  return people.some(child => child.mother === person.name);
 }
 
 /**
@@ -56,6 +79,23 @@ function calculateWomenAverageAge(people, withChildren) {
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
   // write code here
+  const children = (onlyWithSon)
+    ? people.filter(child => getChildren(people, child) && child.sex === 'm')
+    : people.filter(child => getChildren(people, child));
+
+  const ages = children.map(child => getDiffAge(children, people, child));
+
+  return ages.reduce((prev, age) => prev + age) / children.length;
+}
+
+function getChildren(people, child) {
+  return people.some(mother => child.mother === mother.name);
+}
+
+function getDiffAge(children, people, child) {
+  const motherBirth = people.find(mother => mother.name === child.mother);
+
+  return child.born - motherBirth.born;
 }
 
 module.exports = {
