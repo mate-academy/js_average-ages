404fc8f6b173e682bbb100363e91f4959521f879
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-09-21 19:26:27 +0300

    Comments were removed

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,23 +1,16 @@
 'use strict';
 
 /**
- * Implement calculateMenAverageAge function
- *
- * Function returns average age of men in array. If `century` is specified then
- * function calculates average age only for men who died in this century
- *
- * To calculate century:
- * Divide year of person's death by 100: Math.ceil(person.died / 100)
- *
  * @param {object[]} people
  * @param {number} century - optional
  *
  * @return {number}
  */
-function calculateAverageAge(people) {
-  const sumOfAge = people.reduce((prev, age) => prev + age, 0);
 
-  return sumOfAge / people.length;
+function calculateAverageAge(ages) {
+  const sumOfAge = ages.reduce((prev, age) => prev + age, 0);
+
+  return sumOfAge / ages.length;
 }
 
 function calculateMenAverageAge(people, century) {
@@ -32,21 +25,7 @@ function calculateMenAverageAge(people, century) {
   return calculateAverageAge(ages);
 }
 
-// write code here
-// learn how to use array methods like .filter .map .some .every .find .reduce
-// avoid using loop and forEach
-// replace `if ()` statement with &&, || or ?:
-// without nesting
-
 /**
- * Implement calculateWomenAverageAge function
- *
- * Function returns average age of women in array. If `withChildren` is
- * specified then function calculates average age only for women with children
- *
- * Hint: To check if a woman has children you should find someone who mention
- * her as mother.
- *
  * @param {object[]} people
  * @param {boolean} withChildren - optional
  *
@@ -66,15 +45,6 @@ function calculateWomenAverageAge(people, withChildren) {
 }
 
 /**
- * Implement calculateAverageAgeDiff function.
- *
- * The function returns an average age difference
- * between a child and his or her
- * mother in the array. (A mother's age at child birth)
- *
- * If `onlyWithSon` is specified then function calculates age difference only
- * for sons and their mothers.
- *
  * @param {object[]} people
  * @param {boolean} onlyWithSon - optional
  *
