5cf97dabc2ccab8f83f4bfd817c58077c70c08c9
Dmytri Stepanov <wootjkeee@gmail.com>
2022-09-22 14:46:10 +0300

    fixed brackets

--- src/averageAges.js
+++ src/averageAges.js
@@ -2,7 +2,8 @@
 
 function avarageAge(persons) {
   const ages = persons.reduce(
-    (sum, person) => sum + (person.died - person.born),
+    (sum, person) => (
+      sum + (person.died - person.born)),
     0
   );
 
@@ -12,30 +13,35 @@ function avarageAge(persons) {
 }
 
 function calculateMenAverageAge(people, century) {
-  const men = people.filter((person) => century
-    ? person.sex === 'm' && Math.ceil(person.died / 100) === century
-    : person.sex === 'm'
-  );
+  const men = people.filter((person) => (
+    century
+      ? person.sex === 'm' && Math.ceil(person.died / 100) === century
+      : person.sex === 'm'
+  ));
 
   return avarageAge(men);
 }
 
 function calculateWomenAverageAge(people, withChildren) {
-  const women = people.filter((person) => withChildren
-    ? people.find((child) => child.mother === person.name)
-    : person.sex === 'f'
-  );
+  const women = people.filter((person) => (
+    withChildren
+      ? people.find((child) => child.mother === person.name)
+      : person.sex === 'f'
+  ));
 
   return avarageAge(women);
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const hasMother = people.filter((child) => onlyWithSon
-    ? child.mother
-      && people.find((person) => person.name === child.mother)
-      && child.sex === 'm'
-    : child.mother && people.find((person) => person.name === child.mother)
-  );
+  const hasMother = people.filter((child) => (
+    onlyWithSon
+      ? child.mother
+      && people.find((person) => (
+        person.name === child.mother))
+        && child.sex === 'm'
+      : child.mother && people.find((person) => (
+        person.name === child.mother)
+      )));
 
   const ages = hasMother.reduce((sum, person) => {
     const ageDifference = (person.born - people.find((mother) =>
