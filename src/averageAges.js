37bceeac6877127bfa2351f13d6c52878dc02e5d
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-22 19:50:59 +0300

    whatWeFilter was replaced with listOfPeope and list in functions

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,25 +1,25 @@
 'use strict';
 
 function calculateMenAverageAge(people, century) {
-  const whatWeFiler = people.filter(person => (
+  const listOfPeople = people.filter(person => (
     century === undefined
       ? person.sex === 'm'
       : person.sex === 'm' && Math.ceil(person.died / 100) === century
   ));
 
-  const ageDiff = ageDifference(whatWeFiler);
+  const ageDiff = ageDifference(listOfPeople);
 
   return calculateAverageAge(ageDiff);
 }
 
 function calculateWomenAverageAge(people, withChildren) {
-  const whatWeFiler = people.filter(person => (
+  const listOfPeople = people.filter(person => (
     withChildren === undefined
       ? person.sex === 'f'
       : person.sex === 'f' && people.some(some => some.mother === person.name)
   ));
 
-  const ageDiff = ageDifference(whatWeFiler);
+  const ageDiff = ageDifference(listOfPeople);
 
   return calculateAverageAge(ageDiff);
 }
@@ -48,10 +48,10 @@ function calculateAverageAge(data) {
   return averageAge / data.length;
 }
 
-function ageDifference(whatWeFiler) {
+function ageDifference(list) {
   const ageDiff = [];
 
-  whatWeFiler.forEach(person => {
+  list.forEach(person => {
     ageDiff.push(person.died - person.born);
   });
 
