f4c0f9aa83f85db8c23a68b764db88f10a482272
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-07 23:01:54 +0200

    changes after review

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,45 +1,49 @@
 'use strict';
 
 function calculateMenAverageAge(people, century) {
-  const findMen = person => person.sex === 'm';
   const findDeadInRange = person => Math.ceil(person.died / 100) === century;
-  const lifeSpan = (acc, person) => acc + (person.died - person.born);
 
-  const everyDeadMen = people.filter(findMen);
-  const deadList = century
-    ? everyDeadMen.filter(findDeadInRange)
-    : everyDeadMen;
-  const avgAge = deadList.reduce(lifeSpan, 0) / deadList.length;
+  const everyDeadMan = getPeopleBySex('m', people);
+
+  const listToCount = century
+    ? everyDeadMan.filter(findDeadInRange)
+    : everyDeadMan;
+  const avgAge = getLifeSpan(listToCount) / listToCount.length;
 
   return avgAge;
 }
 
 function calculateWomenAverageAge(people, withChildren) {
-  const findWomen = person => person.sex === 'f';
   const findMoms = woman => (people.some(kid => kid.mother === woman.name));
-  const lifeSpan = (acc, person) => acc + (person.died - person.born);
 
-  const everyWoman = people.filter(findWomen);
-  const womenList = withChildren
+  const everyWoman = getPeopleBySex('f', people);
+  const listToCount = withChildren
     ? everyWoman.filter(findMoms)
     : everyWoman;
-  const avgAge = womenList.reduce(lifeSpan, 0) / womenList.length;
+  const avgAge = getLifeSpan(listToCount) / listToCount.length;
 
   return avgAge;
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const findBoys = person => person.sex === 'm';
   const findMoms = child => people.find(woman => child.mother === woman.name);
   const findAgeDiff = (acc, kid) => acc + kid.born - findMoms(kid).born;
 
   const everyChild = people.filter(findMoms);
-  const childList = onlyWithSon
-    ? everyChild.filter(findBoys)
+  const listToCount = onlyWithSon
+    ? getPeopleBySex('m', everyChild)
     : everyChild;
-  const avgDiff = childList.reduce(findAgeDiff, 0) / childList.length;
+  const avgDiff = listToCount.reduce(findAgeDiff, 0) / listToCount.length;
 
   return avgDiff;
+};
+
+function getPeopleBySex(sex, list) {
+  return list.filter(person => person.sex === sex);
+};
+
+function getLifeSpan(list) {
+  return list.reduce((acc, person) => acc + (person.died - person.born), 0);
 };
 
 module.exports = {
