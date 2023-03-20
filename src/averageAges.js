46bb3563a527ae66b31e5dec2dc62b0d1118b4d8
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-20 14:54:23 +0200

    Code style improvements

--- src/averageAges.js
+++ src/averageAges.js
@@ -2,8 +2,12 @@
 
 function calculateMenAverageAge(people, century) {
   const men = century
-    ? people.filter(el => el.sex === 'm'
-      && Math.ceil(el.died / 100) === century)
+    ? people.filter(el => {
+      const isMan = el.sex === 'm';
+      const fitCentury = Math.ceil(el.died / 100) === century;
+
+      return isMan && fitCentury;
+    })
     : people.filter(el => el.sex === 'm');
 
   const menSumAge = men.reduce((acc, el) => acc + el.died - el.born, 0);
@@ -13,8 +17,12 @@ function calculateMenAverageAge(people, century) {
 
 function calculateWomenAverageAge(people, withChildren) {
   const women = withChildren
-    ? people.filter(el => el.sex === 'f'
-      && people.some(potentialKiddo => potentialKiddo.mother === el.name))
+    ? people.filter(el => {
+      const isWoman = el.sex === 'f';
+      const hasChild = people.some(potentKid => potentKid.mother === el.name);
+
+      return isWoman && hasChild;
+    })
     : people.filter(el => el.sex === 'f');
 
   const womenSumAge = women.reduce((acc, el) => acc + el.died - el.born, 0);
@@ -24,13 +32,15 @@ function calculateWomenAverageAge(people, withChildren) {
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
   const kiddos = onlyWithSon
-    ? people.filter(el =>
-      people.some(potentialMother => potentialMother.name === el.mother
-        && el.sex === 'm'
-      ))
-    : people.filter(el =>
+    ? people.filter(el => {
+      const isChild = people.some(potentMom => potentMom.name === el.mother);
+      const isMan = el.sex === 'm';
+
+      return isChild && isMan;
+    })
+    : people.filter(el => (
       people.some(potentialMother => potentialMother.name === el.mother)
-    );
+    ));
 
   const kidsMomsAgeDifference = kiddos.reduce((acc, el) => {
     const mother = people.find(woman => woman.name === el.mother);
