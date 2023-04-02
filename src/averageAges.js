efa14312f7e895fbd288e905d811f52d37e65dd1
Valery Sazonov <vksazonov@gmail.com>
2023-04-02 16:30:54 +0300

    Changed variable names

--- src/averageAges.js
+++ src/averageAges.js
@@ -2,16 +2,12 @@
 
 /**
  * Implement calculateMenAverageAge function
- *
  * Function returns average age of men in array. If `century` is specified then
  * function calculates average age only for men who died in this century
- *
  * To calculate century:
  * Divide year of person's death by 100: Math.ceil(person.died / 100)
- *
  * @param {object[]} people
  * @param {number} century - optional
- *
  * @return {number}
  */
 
@@ -32,16 +28,12 @@ function averageAge(people) {
 }
 /**
  * Implement calculateWomenAverageAge function
- *
  * Function returns average age of women in array. If `withChildren` is
  * specified then function calculates average age only for women with children
- *
  * Hint: To check if a woman has children you should find someone who mention
  * her as mother.
- *
  * @param {object[]} people
  * @param {boolean} withChildren - optional
- *
  * @return {number}
  */
 
@@ -55,16 +47,12 @@ function calculateWomenAverageAge(people, withChildren) {
 }
 /**
  * Implement calculateAverageAgeDiff function.
- *
  * The function returns an average age difference between a child and his or her
  * mother in the array. (A mother's age at child birth)
- *
  * If `onlyWithSon` is specified then function calculates age difference only
  * for sons and their mothers.
- *
  * @param {object[]} people
  * @param {boolean} onlyWithSon - optional
- *
  * @return {number}
  */
 
