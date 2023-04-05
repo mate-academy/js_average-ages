c5ea600398bf34a6be4a94ddacd0d56df8ea2c73
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-04-05 10:24:23 +0300

    Fix N3

--- src/averageAges.js
+++ src/averageAges.js
@@ -18,24 +18,36 @@ function calculateMenAverageAge(people, century) {
 }
 
 /*
+ * Implement calculateWomenAverageAge function
  *
+ * Function returns average age of women in array. If `withChildren` is
+ * specified then function calculates average age only for women with children
+ *
+ * Hint: To check if a woman has children you should find someone who mention
+ * her as mother.
  * @param {object[]} people
  * @param {boolean} withChildren - optional
  *
  * @return {number}
  */
-function calculateWomenAverageAge(people, withChildren = false) {
-  const findWomen = people.filter(item => item.sex === 'f');
-  const motherNames = people.map(item => item.mother);
+function calculateWomenAverageAge(people, withChildren) {
+  const findWomen = people.filter(person => {
+    if (person.sex === 'f') {
+      if (withChildren) {
+        const hasChildren = people.some(item => item.mother === person.name);
 
-  const selectedWomen = findWomen.filter(item =>
-    motherNames.includes(item.name));
+        return hasChildren;
+      }
 
-  const sumOfAges = (withChildren === true)
-    ? selectedWomen.map(item => item.died - item.born)
-    : findWomen.map(item => item.died - item.born);
+      return true;
+    }
 
-  return sumOfAges.reduce((a, b) => (a + b), 0) / sumOfAges.length;
+    return false;
+  });
+
+  return findWomen.reduce((a, b) => {
+    return a + (b.died - b.born);
+  }, 0) / findWomen.length;
 }
 
 /**
