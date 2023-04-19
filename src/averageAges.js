93fe6684360258f21707f3bb8ac6daa1e30720ab
spencerspe <spencerspe@gmail.com>
2023-04-19 14:53:02 +0100

    first solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,21 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+  const men = people.filter(person => person.sex === 'm');
+  const peopleOfCentury = people.filter(person =>
+    (Math.ceil(person.died / 100))
+    === century && person.sex === 'm');
+
+  if (century) {
+    return (peopleOfCentury.map(person =>
+      person.died - person.born)
+      .reduce((a, b) => a + b, 0)
+      / peopleOfCentury.length);
+  } else {
+    return men.map(person => person.died - person.born)
+      .reduce((a, b) => a + b, 0)
+        / men.length;
+  }
 }
 
 /**
@@ -38,6 +53,19 @@ function calculateMenAverageAge(people, century) {
  */
 function calculateWomenAverageAge(people, withChildren) {
   // write code here
+  const women = people.filter(person => person.sex === 'f');
+  const mothers = people.map(person => person.mother);
+  const mothersInList = women.filter(mother => mothers.includes(mother.name));
+
+  if (withChildren) {
+    return mothersInList.map(person => person.died - person.born)
+      .reduce((a, b) => a + b, 0)
+        / mothersInList.length;
+  } else {
+    return women.map(person => person.died - person.born)
+      .reduce((a, b) => a + b, 0)
+        / women.length;
+  }
 }
 
 /**
@@ -56,6 +84,31 @@ function calculateWomenAverageAge(people, withChildren) {
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
   // write code here
+  const women = people.filter(person => person.sex === 'f');
+  const mothers = people.map(person => person.mother);
+  const mothersInList = women.filter(mum => mothers.includes(mum.name));
+  const sons = people.filter(son => son.sex === 'm');
+
+  let toSearch = people;
+
+  if (onlyWithSon) {
+    toSearch = sons;
+  }
+
+  const motherAge = [];
+
+  for (let i = 0; i < toSearch.length; i++) {
+    for (let j = 0; j < mothersInList.length; j++) {
+      if (toSearch[i].mother === mothersInList[j].name) {
+        motherAge.push(toSearch[i].born - mothersInList[j].born);
+      }
+    }
+  }
+
+  const motherAverageAge = motherAge.reduce((a, b) =>
+    a + b, 0) / motherAge.length;
+
+  return Math.round(((motherAverageAge) + Number.EPSILON) * 100) / 100;
 }
 
 module.exports = {
