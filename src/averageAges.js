2d23d2e44311c95c568bf4c654cf5066221e2e58
AlexKherson <q0992358472@gmail.com>
2023-05-15 19:54:17 +0300

    add task solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,13 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+
+  const agesOfMen = people
+    .filter(({ sex, died }) => sex === 'm' && isDiedInCentury(died, century))
+    .map(person => person.died - person.born);
+  const totalAges = agesOfMen.reduce((a, b) => a + b, 0);
+
+  return (totalAges / agesOfMen.length) || 0;
 }
 
 /**
@@ -37,7 +44,16 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const agesOfWomen = people
+    .filter(person => {
+      const isMother = !withChildren || isHasChildren(people, person.name);
+
+      return person.sex === 'f' && isMother;
+    })
+    .map(person => person.died - person.born);
+  const totalAges = agesOfWomen.reduce((a, b) => a + b, 0);
+
+  return (totalAges / agesOfWomen.length) || 0;
 }
 
 /**
@@ -55,7 +71,33 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // write code here
+  const mothers = people.filter(person => isHasChildren(people, person.name));
+  const children = people
+    .filter(child => {
+      const motherNames = mothers.map(mom => mom.name);
+
+      return motherNames
+        .includes(child.mother) && (!onlyWithSon || child.sex === 'm');
+    });
+  const differenceAges = children.map(child => {
+    const mother = mothers.find(mom => mom.name === child.mother);
+
+    return child.born - mother.born;
+  });
+  const totalDifferenceAges = differenceAges.reduce((a, b) => a + b, 0);
+
+  return (totalDifferenceAges / differenceAges.length) || 0;
+}
+
+function isDiedInCentury(died, initialCentury) {
+  const centuryOfDied = Math.ceil(died / 100);
+  const century = initialCentury || centuryOfDied;
+
+  return centuryOfDied === century;
+}
+
+function isHasChildren(people, motherName) {
+  return people.some(person => person.mother === motherName);
 }
 
 module.exports = {
