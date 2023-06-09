df0d5631854c9a30e124ba94f5724a245fa9295c
grinjk1993 <grinjk1993@gmail.com>
2023-06-09 12:36:56 +0300

    fixed

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,20 +1,5 @@
 'use strict';
 
-/**
- * Implement calculateMenAverageAge function
- *
- * Function returns average age of men in array. If `century` is specified then
- * function calculates average age only for men who died in this century
- *
- * To calculate century:
- * Divide year of person's death by 100: Math.ceil(person.died / 100)
- *
- * @param {object[]} people
- * @param {number} century - optional
- *
- * @return {number}
- */
-
 function calculateAverageAge(people) {
   const totalAge = people.reduce((sum, person) =>
     sum + (person.died - person.born), 0);
@@ -31,20 +16,6 @@ function calculateMenAverageAge(people, century) {
   return calculateAverageAge(men);
 }
 
-/**
- * Implement calculateWomenAverageAge function
- *
- * Function returns average age of women in array. If `withChildren` is
- * specified then function calculates average age only for women with children
- *
- * Hint: To check if a woman has children you should find someone who mention
- * her as mother.
- *
- * @param {object[]} people
- * @param {boolean} withChildren - optional
- *
- * @return {number}
- */
 function calculateWomenAverageAge(people, withChildren) {
   const women = withChildren
     ? people.filter(person => person.sex === 'f')
@@ -54,20 +25,6 @@ function calculateWomenAverageAge(people, withChildren) {
   return calculateAverageAge(women);
 }
 
-/**
- * Implement calculateAverageAgeDiff function.
- *
- * The function returns an average age difference between a child and his or her
- * mother in the array. (A mother's age at child birth)
- *
- * If `onlyWithSon` is specified then function calculates age difference only
- * for sons and their mothers.
- *
- * @param {object[]} people
- * @param {boolean} onlyWithSon - optional
- *
- * @return {number}
- */
 function calculateAverageAgeDiff(people, onlyWithSon) {
   const mothers = people.filter(mother =>
     !onlyWithSon
