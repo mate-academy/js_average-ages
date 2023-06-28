726caef25892db06ef2fb5843946eb1363a8d8c9
Diana Pinchuk <diana9360@gmail.com>
2023-06-28 22:49:21 +0300

    fixed

--- src/averageAges.js
+++ src/averageAges.js
@@ -7,11 +7,11 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  const arr = people.filter((person) => (arguments.length > 1
+  const filteredMen = people.filter((person) => (arguments.length > 1
     ? Math.ceil(person.died / 100) === century && person.sex === 'm'
     : person.sex === 'm'));
 
-  return calculate(arr);
+  return calculate(filteredMen);
 }
 
 /**
@@ -22,12 +22,12 @@ function calculateMenAverageAge(people, century) {
  */
 
 function calculateWomenAverageAge(people, withChildren) {
-  const arr = people.filter((person) => (withChildren
+  const filteredWomen = people.filter((person) => (withChildren
     ? people.find((someone) => someone.mother === person.name)
     && person.sex === 'f'
     : person.sex === 'f'));
 
-  return calculate(arr);
+  return calculate(filteredWomen);
 }
 
 /**
@@ -38,22 +38,21 @@ function calculateWomenAverageAge(people, withChildren) {
  */
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  let arr = people.filter((person) => (onlyWithSon
+  const filteredPeople = people.filter((person) => (onlyWithSon
     ? people.find((someone) => someone.name === person.mother)
     && person.sex === 'm'
     : people.find((someone) => someone.name === person.mother)));
 
-  arr = +(arr.reduce((prev, person) =>
+  return +(filteredPeople.reduce((prev, person) =>
     prev + (person.born - people.find((someone) =>
       someone.name === person.mother).born), 0)
-    / arr.length).toFixed(2);
-
-  return arr;
+    / filteredPeople.length).toFixed(2);
 }
 
-function calculate(arr) {
-  return +(arr.reduce((prev, person) => prev + (person.died - person.born), 0)
-  / arr.length).toFixed(2);
+function calculate(filteredPeople) {
+  return filteredPeople
+    .map(person => person.died - person.born).reduce((prev, person) =>
+      prev + person, 0) / filteredPeople.length.toFixed(2);
 }
 
 module.exports = {
