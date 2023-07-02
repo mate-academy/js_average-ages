23dc60361c82db83a5eb096bdde21618efba7d56
Anton <tony.tarovyk@gmail.com>
2023-07-02 13:35:03 +0300

    add solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,19 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+  let menArr = people.filter(elem => {
+    return elem.sex === 'm';
+  });
+
+  if (century) {
+    menArr = menArr.filter((elem) => Math.ceil(elem.died / 100) === century);
+  }
+
+  const averageAge = menArr.reduce((acc, elem) => {
+    return acc + (elem.died - elem.born);
+  }, 0) / menArr.length;
+
+  return averageAge;
 }
 
 /**
@@ -38,6 +51,21 @@ function calculateMenAverageAge(people, century) {
  */
 function calculateWomenAverageAge(people, withChildren) {
   // write code here
+  let womenArr = people.filter(elem => {
+    return elem.sex === 'f';
+  });
+
+  if (withChildren) {
+    womenArr = womenArr.filter((woman) =>
+      people.some((person) => person.mother === woman.name)
+    );
+  }
+
+  const averageAge = womenArr.reduce((acc, elem) => {
+    return acc + (elem.died - elem.born);
+  }, 0) / womenArr.length;
+
+  return averageAge;
 }
 
 /**
@@ -55,10 +83,36 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // 1. find a mother of each person (or only for men)
-  // 2. keep people who have mothers in the array
-  // 3. calculate the difference child.born - mother.born
-  // 4. return the average value
+  function calculateAverage(arr) {
+    if (arr.length === 0) {
+      return 0;
+    }
+
+    const sum = arr.reduce((total, value) => total + value, 0);
+
+    return sum / arr.length;
+  }
+
+  const mothersArr = people.filter(person => {
+    return people.some(elem => elem.name === person.mother);
+  });
+
+  const ageDiffs = mothersArr.map(mother => {
+    const child = people.find(person => person.name === mother.mother);
+
+    return child.born - mother.born;
+  });
+
+  if (onlyWithSon) {
+    const sonsArr = people.filter(person => person.sex === 'm');
+    const sonAgeDiffs = ageDiffs.filter((_, index) => {
+      return sonsArr.includes(mothersArr[index]);
+    });
+
+    return Math.abs(calculateAverage(sonAgeDiffs));
+  }
+
+  return Math.abs(calculateAverage(ageDiffs));
 }
 
 module.exports = {
