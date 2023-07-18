a91bed61752d7ad8a24083145f9a1a351b9253e2
Alina Kabanets <kabanets.dolls@gmail.com>
2023-07-18 15:33:30 +0100

    Solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -15,7 +15,15 @@
  * @return {number}
  */
 function calculateMenAverageAge(people, century) {
-  // write code here
+  const men = people.filter(({ died, sex }) => century
+    ? sex === 'm' && Math.ceil(died / 100) === century
+    : sex === 'm');
+
+  const averAge = men.reduce((calc, man) =>
+    (calc + (man.died - man.born)), 0) / men.length;
+
+  return averAge;
+
   // learn how to use array methods like .filter .map .some .every .find .reduce
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
@@ -37,7 +45,14 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const women = people.filter(({ name, sex }) => withChildren
+    ? sex === 'f' && people.find(kid => kid.mother === name)
+    : sex === 'f');
+
+  const averAge = women.reduce((calc, woman) =>
+    (calc + (woman.died - woman.born)), 0) / women.length;
+
+  return averAge;
 }
 
 /**
@@ -55,6 +70,19 @@ function calculateWomenAverageAge(people, withChildren) {
  * @return {number}
  */
 function calculateAverageAgeDiff(people, onlyWithSon) {
+  const mothers = people.filter(({ name, sex }) => onlyWithSon
+    ? sex === 'f' && people.find(son => son.sex === 'm' && son.mother === name)
+    : sex === 'f' && people.find(kid => kid.mother === name));
+
+  const kids = people.filter(({ sex, mother }) => onlyWithSon
+    ? sex === 'm' && mothers.some(m => m.name === mother)
+    : mothers.some(m => m.name === mother));
+
+  const averAge = kids.reduce((calc, child) =>
+    (calc + (child.born - mothers.find(m => m.name === child.mother).born)), 0)
+    / kids.length;
+
+  return averAge;
   // 1. find a mother of each person (or only for men)
   // 2. keep people who have mothers in the array
   // 3. calculate the difference child.born - mother.born
