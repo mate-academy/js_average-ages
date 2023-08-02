be5cf0e6c90084770ac7b257b6e75b0209949962
Piotr <piotrkrasucki@gmail.com>
2023-08-02 11:57:48 +0200

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -20,6 +20,20 @@ function calculateMenAverageAge(people, century) {
   // avoid using loop and forEach
   // replace `if ()` statement with &&, || or ?:
   // without nesting
+
+  // ternary operator result-> put in men array
+  // ? if century is given find all male person died in this century
+  // : if century is not given, find all male person
+  const men = century
+    ? people.filter(person => Math.ceil(person.died / 100)
+    === century && person.sex === 'm')
+    : people.filter(person => person.sex === 'm');
+  // sum given men age
+  const totalAge = men.reduce((acc, person) => acc
+   + (person.died - person.born), 0);
+  // return average age if array is not empty
+
+  return men.length > 0 ? totalAge / men.length : 0;
 }
 
 /**
@@ -37,7 +51,21 @@ function calculateMenAverageAge(people, century) {
  * @return {number}
  */
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  // write code here// if specified withchildren:
+  // ? put in array women, all women from ternary op. result
+  // if some person has specified given women as mother,
+  // given women is withchildren
+  // : if not specified withchildren , we need all women
+  const women = withChildren
+    ? people.filter(person => person.sex === 'f' && people.some(p =>
+      p.mother === person.name))
+    : people.filter(person => person.sex === 'f');
+    // sum total womens age
+  const totalAge = women.reduce((acc, person) => acc
+   + (person.died - person.born), 0);
+    // if women array is not empty return average age
+
+  return women.length > 0 ? totalAge / women.length : 0;
 }
 
 /**
@@ -59,6 +87,29 @@ function calculateAverageAgeDiff(people, onlyWithSon) {
   // 2. keep people who have mothers in the array
   // 3. calculate the difference child.born - mother.born
   // 4. return the average value
+
+  // if only with son
+  // ? children is array male people that have some mother
+  // : if parameter not specified children is an array
+  // male and female people, with some mother
+  const children = onlyWithSon
+    ? people.filter(person => person.sex === 'm'
+    && people.some(mother => mother.name === person.mother))
+    : people.filter(person => people
+      .some(mother => mother.name === person.mother));
+  // find all person they are mother for somebody from array children
+  // if mother exist for child, return age difference
+  // if mother not exist return 0;
+  const sumOfTheAgeDifferences = children.reduce((acc, child) => {
+    const mother = people.find(person => person.name === child.mother);
+    const ageDifference = mother ? child.born - mother.born : 0;
+    // sum age differences
+
+    return acc + ageDifference;
+  }, 0);
+  // return average age difference for children
+
+  return sumOfTheAgeDifferences / children.length;
 }
 
 module.exports = {
