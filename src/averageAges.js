d6b14b3c68a57b226bb9367b12f9ad0308607a44
piotr <piotr_as@yahoo.com>
2023-08-03 16:06:25 +0200

    solution

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const people = require('./people');
+
 /**
  * Implement calculateMenAverageAge function
  *
@@ -14,12 +16,28 @@
  *
  * @return {number}
  */
+
+//   // write code here
+//   // learn how to use array methods like
+//  .filter .map .some .every .find .reduce
+//   // avoid using loop and forEach
+//   // replace `if ()` statement with &&, || or ?:
+//   // without nesting
+
 function calculateMenAverageAge(people, century) {
-  // write code here
-  // learn how to use array methods like .filter .map .some .every .find .reduce
-  // avoid using loop and forEach
-  // replace `if ()` statement with &&, || or ?:
-  // without nesting
+  const MEN = people.filter((person) => person.sex === 'm');
+  const MEN_IN_CENTURY = MEN
+    .filter((person) => Math.ceil(person.died / 100) === century);
+  const ALL_MEN_IN_CENTURY_AGE = MEN_IN_CENTURY
+    .map(person => (person.died - person.born));
+  const AGES_SUM = ALL_MEN_IN_CENTURY_AGE.reduce((a, b) => a + b, 0);
+  const AVERAGE_AGE_WITH_CENTURY = AGES_SUM / ALL_MEN_IN_CENTURY_AGE.length;
+  const AVERAGE_MEN_AGE = MEN.map(person => (person.died - person.born))
+    .reduce((a, b) => a + b, 0) / MEN.length;
+
+  return century !== undefined
+    ? Number(AVERAGE_AGE_WITH_CENTURY.toFixed(2))
+    : Number(AVERAGE_MEN_AGE.toFixed(2));
 }
 
 /**
@@ -36,8 +54,22 @@ function calculateMenAverageAge(people, century) {
  *
  * @return {number}
  */
+
 function calculateWomenAverageAge(people, withChildren) {
-  // write code here
+  const haveChildren = (all, name) =>
+    all.some(person => person.mother === name);
+
+  const WOMEN = withChildren
+    ? people.filter(person =>
+      (person.sex === 'f' && haveChildren(people, person.name)))
+    : people.filter(person => person.sex === 'f');
+
+  const WOMEN_TOTAL_AGE = WOMEN
+    .reduce((v, person) => v + (person.died - person.born), 0);
+
+  const WOMEN_AVERAGE_AGE = WOMEN_TOTAL_AGE / WOMEN.length;
+
+  return Number(WOMEN_AVERAGE_AGE.toFixed(2));
 }
 
 /**
@@ -54,11 +86,29 @@ function calculateWomenAverageAge(people, withChildren) {
  *
  * @return {number}
  */
+
+//   // 1. find a mother of each person (or only for men)
+//   // 2. keep people who have mothers in the array
+//   // 3. calculate the difference child.born - mother.born
+//   // 4. return the average value
+
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  // 1. find a mother of each person (or only for men)
-  // 2. keep people who have mothers in the array
-  // 3. calculate the difference child.born - mother.born
-  // 4. return the average value
+  const ALL = onlyWithSon
+    ? people.filter(person => person.sex === 'm')
+    : people;
+
+  const AGES = ALL
+    .map(person => {
+      const MOTHER = people.find(woman => woman.name === person.mother);
+
+      return MOTHER ? person.born - MOTHER.born : null;
+    })
+    .filter(el => el);
+
+  const AVERAGE_AGE_DIFF = AGES
+    .reduce((a, b) => a + b) / AGES.length;
+
+  return Number(AVERAGE_AGE_DIFF.toFixed(2));
 }
 
 module.exports = {
