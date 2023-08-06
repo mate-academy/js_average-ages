f90d6dacaa50f649116d0f6db9837360887c9c4a
Anton Kuts <anton.a.kuts@gmail.com>
2023-08-06 16:31:00 +0300

    Fixed a bug and optimized the code

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,45 +1,57 @@
 'use strict';
 
 const totalAge = (array) => {
-  return array.reduce(
-    (acc, per) => acc + (per.died - per.born), 0);
+  return array.reduce((acc, per) => acc + (per.died - per.born), 0);
 };
 
-const conditionCheck = (firstCondition, secondCondition) => {
-  return firstCondition === secondCondition;
+const isMale = (perosonSex) => {
+  return perosonSex === 'm';
+};
+
+const isFemale = (perosonSex) => {
+  return perosonSex === 'f';
 };
 
 function calculateMenAverageAge(people, century) {
-  const newPeople = century ? people.filter(
-    person => conditionCheck(person.sex, 'm')
-    && Math.ceil(person.died / 100) === century)
-    : people.filter(person => conditionCheck(person.sex, 'm'));
+  const newPeople = century ? people.filter(person =>
+    isMale(person.sex)
+        && Math.ceil(person.died / 100) === century
+  )
+    : people.filter(person => isMale(person.sex));
 
   return totalAge(newPeople) / newPeople.length;
 }
 
 function calculateWomenAverageAge(people, withChildren) {
   const woman = withChildren
-    ? people.filter(person => conditionCheck(person.sex, 'f')
-    && people.some(mother => conditionCheck(person.name, mother.mother)))
-    : people.filter(person => conditionCheck(person.sex, 'f'));
+    ? people.filter(
+      (person) =>
+        isFemale(person.sex)
+          && people.some((mother) => person.name === mother.mother)
+    )
+    : people.filter((person) => isFemale(person.sex));
 
   return totalAge(woman) / woman.length;
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const children = onlyWithSon
-    ? people.filter(person =>
-      people.some(child => conditionCheck(child.name, person.mother))
-    && conditionCheck(person.sex, 'm'))
-    : people.filter(person =>
-      people.some(child => conditionCheck(child.name, person.mother)));
+  const children = people.filter((person) => {
+    const isMalePerson = isMale(person.sex);
+
+    if (onlyWithSon) {
+      return (
+        people.some((child) => child.name === person.mother) && isMalePerson
+      );
+    }
+
+    return people.some(child => child.name === person.mother);
+  });
 
   let motherAge = 0;
 
-  children.forEach(person => {
-    const mothers = people.find(
-      mother => conditionCheck(mother.name, person.mother));
+  children.forEach((person) => {
+    const mothers = people.find((mother) => mother.name === person.mother
+    );
 
     if (mothers) {
       motherAge += person.born - mothers.born;
