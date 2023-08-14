8182ae47a80b2d13c5cd5485b6b79343dd69c47b
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-08-14 13:43:02 +0300

    add destructuring

--- src/averageAges.js
+++ src/averageAges.js
@@ -1,46 +1,49 @@
 'use strict';
 
-function calculateMenAverageAge(people, century) {
-  const onlyMen = genderFilter(people, 'm');
+const FEMALE_GENDER = 'f';
+const MALE_GENDER = 'm';
 
-  const filteredMen = (century)
-    ? onlyMen.filter(person => Math.ceil(person.died / 100) === century)
+function calculateMenAverageAge(people, century) {
+  const onlyMen = genderFilter(people, MALE_GENDER);
+
+  const filteredMen = century
+    ? onlyMen.filter(({ died }) => Math.ceil(died / 100) === century)
     : onlyMen;
 
   return calculateAvgAge(filteredMen);
 }
 
 function calculateWomenAverageAge(people, withChildren) {
-  const onlyWomen = genderFilter(people, 'f');
+  const onlyWomen = genderFilter(people, FEMALE_GENDER);
 
-  const filteredWomen = (withChildren)
-    ? onlyWomen.filter(woman => people.some(p => p.mother === woman.name))
+  const filteredWomen = withChildren
+  // eslint-disable-next-line
+    ? onlyWomen.filter(({ name }) => people.some(({ mother }) => mother === name))
     : onlyWomen;
 
   return calculateAvgAge(filteredWomen);
 }
 
 function calculateAverageAgeDiff(people, onlyWithSon) {
-  const filteredChildren = people.filter(person => (onlyWithSon)
-    ? people.find(mother => mother.name === person.mother
-      && person.sex === 'm')
-    : people.find(mother => mother.name === person.mother)
+  const filteredChildren = people.filter(({ mother, sex }) => (onlyWithSon)
+    ? people.find(({ name }) => name === mother && sex === MALE_GENDER)
+    : people.find(({ name }) => name === mother)
   );
 
-  const ageDiff = filteredChildren.map(child =>
-    child.born - people.find(mother => mother.name === child.mother).born);
+  const ageDiff = filteredChildren.map(({ born, mother }) =>
+    born - people.find(({ name }) => name === mother).born);
 
   return ageDiff.reduce((acc, age) => acc + age) / ageDiff.length;
 }
 
 function calculateAvgAge(people) {
-  return people
-    .reduce((acc, person) =>
-      acc + (person.died - person.born), 0) / people.length;
+  return people.reduce((acc, { died, born }) => (
+    acc + (died - born)
+  ), 0) / people.length;
 }
 
 function genderFilter(people, gender) {
-  return people.filter(person => person.sex === gender);
+  return people.filter(({ sex }) => sex === gender);
 }
 
 module.exports = {
